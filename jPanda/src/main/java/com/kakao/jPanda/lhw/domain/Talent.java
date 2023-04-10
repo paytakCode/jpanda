4437650b6b1209413d4746e9e63a5a0509c16635
user <user@DESKTOP-2PGRMF4>
2023-04-10 17:14:27 +0900

    domain 추가
    23.04.10

---
+++ jPanda/src/main/java/com/kakao/jPanda/lhw/domain/Talent.java
@@ -0,0 +1,21 @@
+package com.kakao.jPanda.lhw.domain;
+
+import lombok.Data;
+
+@Data
+public class Talent {
+	private Long talentNo;
+	private String sellerId;
+	private Long categoryNo;
+	private Long categoryNo2;
+	private String mainImg;
+	private String title;
+	private Long content;
+	private Long bamboo;
+	private Long saleBamboo;
+	private String summary;
+	private String status;
+	private Long viewCount;
+	private String regDate;
+	
+}
