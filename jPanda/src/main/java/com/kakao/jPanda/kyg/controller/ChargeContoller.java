d4b8b53ee2fab2b2ed44db52e078c1a3a4ed84d5
qwe20 <qwe20@User>
2023-04-11 22:19:58 +0900

    navigation sample

--- jPanda/src/main/java/com/kakao/jPanda/kyg/controller/ChargeContoller.java
+++ jPanda/src/main/java/com/kakao/jPanda/kyg/controller/ChargeContoller.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
 @Controller
 public class ChargeContoller {
 
@@ -17,5 +17,6 @@ public class ChargeContoller {
 	}
 	
 	
-	
 }
+
+*/
