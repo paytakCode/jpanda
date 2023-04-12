27020b59d52ac112ef621710ebaca7f937bd892b
paytakcode <paytakcode@gmail.com>
2023-04-12 17:33:51 +0900

    footer 작성중

--- jPanda/src/main/java/com/kakao/jPanda/kts/controller/Test.java
+++ jPanda/src/main/java/com/kakao/jPanda/kts/controller/Test.java
@@ -1,5 +1,13 @@
 package com.kakao.jPanda.kts.controller;
 
-public class Test {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class Test {
+	
+	@GetMapping("/footer")
+	public String footerTest() {
+		return "/";
+	}
 }
