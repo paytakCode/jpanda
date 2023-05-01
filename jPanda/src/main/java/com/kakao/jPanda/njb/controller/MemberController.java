c554cb75dbeaa1569299fd1273c073850237b1c3
jb <jb@localhost>
2023-05-01 10:28:52 +0900

    ERD수정 후 로그인 기능 에러수정

--- jPanda/src/main/java/com/kakao/jPanda/njb/controller/MemberController.java
+++ jPanda/src/main/java/com/kakao/jPanda/njb/controller/MemberController.java
@@ -138,13 +138,13 @@ public class MemberController {
     
     @PostMapping("/login")
     public String login(@ModelAttribute MemberDto memberDto, HttpSession session,RedirectAttributes redirectAttributes) {
-    	System.out.println(memberDto.getId() + memberDto.getPassword());
+    	System.out.println(memberDto.getMemberId() + memberDto.getPassword());
     	boolean loginResult = memberservice.login(memberDto);
     	if(loginResult) {
-    		session.setAttribute("loginId", memberDto.getId());
+    		session.setAttribute("loginId", memberDto.getMemberId());
     		String loginId = (String) session.getAttribute("loginId");
     		System.out.println(loginId);
-            MemberDto member = memberservice.selectMember(memberDto.getId());
+            MemberDto member = memberservice.selectMember(memberDto.getMemberId());
             
            
             redirectAttributes.addFlashAttribute("memberInfo", member);
