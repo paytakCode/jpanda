ef23b97c06f3e4154487b9ddaa3ae54d6e3b78cc
paytakcode <paytakcode@gmail.com>
2023-05-02 00:32:58 +0900

    Chat-layout 생성
    chat-layout 페이지는 로그인 시 해당 아이디의 채팅이 로딩됨
    ChatMessage 제거 및 Chat으로 대체
    chat icon 추가
    chat 내부 스타일 수정중

--- jPanda/src/main/java/com/kakao/jPanda/kts/controller/ChatController.java
+++ jPanda/src/main/java/com/kakao/jPanda/kts/controller/ChatController.java
@@ -1,10 +1,8 @@
 package com.kakao.jPanda.kts.controller;
 
 import java.util.List;
-
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.kakao.jPanda.kts.domain.Chat;
-import com.kakao.jPanda.kts.domain.ChatMessage;
 import com.kakao.jPanda.kts.domain.Member;
 import com.kakao.jPanda.kts.domain.Partner;
 import com.kakao.jPanda.kts.service.ChatService;
-
+import com.kakao.jPanda.njb.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -31,11 +28,13 @@ public class ChatController {
 	
 	private final SqlSession sqlsession; // 임시
 	private final ChatService chatService;
+	private final MemberService memberservice;
 	
 	@Autowired
-	public ChatController(ChatService chatService, SqlSession sqlsession) {
+	public ChatController(ChatService chatService, SqlSession sqlsession, MemberService memberservice) {
 		this.chatService = chatService;
 		this.sqlsession = sqlsession;
+		this.memberservice = memberservice;
 	}
 	
 	@GetMapping("/chat-test")
@@ -45,11 +44,11 @@ public class ChatController {
 	}
 	
 	@ResponseBody
-	@GetMapping("/chats/{memberId}/chatMessages")
-	public List<ChatMessage> chatMessageListByMemberId(@PathVariable String memberId){
-		log.info("[chatMessageListByMemberId]");
-		List<ChatMessage> foundChatMessageList = chatService.findChatMessageListByMemberId(memberId);	
-		return foundChatMessageList;
+	@GetMapping("/chats")
+	public List<Chat> chatListByMemberId(@RequestParam String memberId){
+		log.info("[chatListByMemberId]");
+		List<Chat> foundChatList = chatService.findChatListByMemberId(memberId);	
+		return foundChatList;
 	}
 	
 	@ResponseBody
@@ -62,9 +61,9 @@ public class ChatController {
 	
 	@MessageMapping("/message")
 	@SendToUser(value = "/direct/{memberId}", broadcast = false)
-	public ChatMessage sendMessage(ChatMessage message) {
-		log.info("[sendMessage] MessageInfo {}", message.toString());
-		return message;
+	public Chat sendMessage(Chat chat) {
+		log.info("[sendMessage] chatInfo {}", chat.toString());
+		return chat;
 	}
 	
 	@ResponseBody
@@ -86,5 +85,22 @@ public class ChatController {
 		log.info("[memberList]");
 		List<Member> memberList = sqlsession.selectList("selectMemberList");
 		return memberList;
+	}
+	
+	//모든 계정 아이디와 비밀번호 동일하게 변경하는 임시 메소드
+	@ResponseBody
+	@GetMapping("/members-init-pwd")
+	public String memberPasswordInit() {
+	    List<Member> memberList = sqlsession.selectList("selectMemberList");
+	    for(Member member : memberList) {
+	        memberservice.updatePasswordById(member.getMemberId(), member.getMemberId());
+	    }
+	    return "End";
+	}
+	
+	//chat-layout Test용
+	@GetMapping("/chat-layout")
+	public String chatLayout() {
+	    return "/common/chat-sample";
 	}
 }
