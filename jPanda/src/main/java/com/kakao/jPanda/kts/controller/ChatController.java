e60013163ac4c061297c4f4343096dc47dc32acc
paytakcode <paytakcode@gmail.com>
2023-05-05 16:31:54 +0900

    Chat 일부 로직 수정 및 Log 상세화
    채팅 상대와 무관하게 항상 로그인한 멤버의 모든 채팅을 가져와서 선택한 상대와의 채팅으로 Filtering 하였으나
    추후 채팅 기록이 많으면 많을수록 비효율적인 과정을 거치는 것이므로
    채팅 상대와 로그인한 멤버와의 채팅만 가져오는 로직으로 수정
    
    Log를 통해 chat 기능 상태를 확인하기 위해 상세한 내용 기입
    연결 시도 log를 추가하기위해 HandShakeInterceptor 추가

--- jPanda/src/main/java/com/kakao/jPanda/kts/controller/ChatController.java
+++ jPanda/src/main/java/com/kakao/jPanda/kts/controller/ChatController.java
@@ -1,9 +1,7 @@
 package com.kakao.jPanda.kts.controller;
 
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -15,11 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.kakao.jPanda.kts.domain.Chat;
 import com.kakao.jPanda.kts.domain.Partner;
 import com.kakao.jPanda.kts.service.ChatService;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -32,19 +28,19 @@ public class ChatController {
 	public ChatController(ChatService chatService) {
 		this.chatService = chatService;
 	}
-	
-	@ResponseBody
-	@GetMapping("/chats")
-	public List<Chat> chatListByMemberId(@RequestParam String memberId){
-		log.info("[chatListByMemberId]");
-		List<Chat> foundChatList = chatService.findChatListByMemberId(memberId);	
-		return foundChatList;
-	}
+    
+    @ResponseBody
+    @GetMapping("/chats/{memberId}")
+    public List<Chat> chatListByMemberIdAndPartnerId(@PathVariable String memberId, @RequestParam(name = "partner-id") String partnerId){
+        log.info("[chatListByMemberIdAndPartnerId] {}의 상대 {}와의 채팅 정보를 불러옵니다.", memberId, partnerId);
+        List<Chat> foundChatList = chatService.findChatListByMemberIdAndPartnerId(memberId, partnerId);    
+        return foundChatList;
+    }
 	
 	@ResponseBody
 	@GetMapping("/chats/{memberId}/partners")
 	public List<Partner> partnerListByMemberId(@PathVariable String memberId){
-		log.info("[partnerListByMemberId]");
+		log.info("[partnerListByMemberId] {}의 채팅 상대 목록을 불러옵니다.", memberId);
 		List<Partner> foundPartnerList = chatService.findPartnerListByMemberId(memberId);
 		return foundPartnerList;
 	}
@@ -52,18 +48,20 @@ public class ChatController {
 	@MessageMapping("/message")
 	@SendToUser(value = "/direct/{memberId}", broadcast = false)
 	public Chat sendMessage(Chat chat) {
-		log.info("[sendMessage] chatInfo {}", chat.toString());
+		log.info("[sendMessage] 메세지를 전송합니다. {}", chat.toString());
 		return chat;
 	}
 	
 	@ResponseBody
 	@PostMapping("/chat")
 	public ResponseEntity<Integer> chatSave(@Valid @RequestBody Chat chat){
-		log.info("[chatSave]");
+		log.info("[chatSave] 메세지를 DB에 저장합니다. {}", chat.toString());
 		Integer result = chatService.saveChat(chat);
 		if(result == 1) {
+		    log.info("[chatSave] 저장 완료 {}", result);
 			return ResponseEntity.accepted().body(result);
 		} else {
+            log.info("[chatSave] 저장 실패 {}", result);
 			return ResponseEntity.internalServerError().body(result);
 		}
 	}
