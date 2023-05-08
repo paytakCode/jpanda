419d2d4a477a96b53d5bfb2103913e29e667331d
paytakcode <paytakcode@gmail.com>
2023-05-08 12:23:58 +0900

    Chat 개선
    1.불필요한 DTO 제거(member), configuration 수정
    2.JS console 수정
    3.채팅 신고 후 신고 사유 초기화 오류 수정

--- jPanda/src/main/java/com/kakao/jPanda/kts/service/ChatService.java
+++ jPanda/src/main/java/com/kakao/jPanda/kts/service/ChatService.java
@@ -3,14 +3,14 @@ package com.kakao.jPanda.kts.service;
 import java.util.List;
 import java.util.Map;
 import com.kakao.jPanda.kts.domain.Chat;
-import com.kakao.jPanda.kts.domain.Partner;
+import com.kakao.jPanda.kts.domain.ChatPartner;
 import com.kakao.jPanda.kts.domain.ChatReport;
 
 public interface ChatService {
 
 	int saveChat(Chat chat);
 
-	List<Partner> findPartnerListByMemberId(String memberId);
+	List<ChatPartner> findPartnerListByMemberId(String memberId);
 
     List<Chat> findChatListByMemberIdAndPartnerId(String memberId, String partnerId);
 
